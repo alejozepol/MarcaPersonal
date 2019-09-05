c0d4988e0884f83825cc4b256cba7aff316e8a6d
Alejandro Lopez <alejozepol@gmail.com>
2019-09-05 01:43:32 -0500

    creacion experiencia laboral en CV

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -7,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CVComponent implements OnInit {
 
+  experienciaLaboral:any =[
+    {fechaInicio: 'Agosto/2018',
+    fechaFin:'Julio/2019',
+    cargo:'Co-fundador y CFO',
+    empresa:'Undervisa Master Trade',
+    actividad:[
+      '-Planificación financiera',
+      '-Administración de redes sociales',
+      '-Elaboracion  de campañas de marketing',
+      '-Elaboraciones de planes de Negocios',
+      '-Desarrollo de web application PWA',
+      '-Elaboracion y administracion de Paginas Web'
+    ],
+    logo:'../assets/Undervisa.svg'
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
