c20eedc8a647dd9ff1b1d75e982f13fd7da4465a
Alejandro Lopez <alejozepol@gmail.com>
2019-09-05 00:49:13 -0500

    creacion cv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { Routes, RouterModule } from '@angular/router';
 import { QuienSoyComponent } from './quien-soy/quien-soy.component';
+import { CVComponent } from './cv/cv.component';
 
 const appRoutes: Routes =[
   {path:'', component: InicioComponent},
   {path:'inicio', component: InicioComponent},
-  {path:'QuienSoy', component: QuienSoyComponent}
+  {path:'QuienSoy', component: QuienSoyComponent},
+  {path:'cv', component: CVComponent}
 ]
 
 @NgModule({
@@ -18,6 +20,7 @@ const appRoutes: Routes =[
     AppComponent,
     InicioComponent,
     QuienSoyComponent,
+    CVComponent,
   ],
   imports: [
     BrowserModule,
