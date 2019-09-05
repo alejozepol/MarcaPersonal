48f35c59fbba81cd0e10fdb8bea417a98239e370
Alejandro Lopez <alejozepol@gmail.com>
2019-09-05 03:41:43 -0500

    alimentacion de experiencia laboral

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -8,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class CVComponent implements OnInit {
 
   experienciaLaboral:any =[
-    {fechaInicio: 'Agosto/2018',
+    {empresa:'Undervisa Master Trade',
+      fechaInicio: 'Agosto/2018',
     fechaFin:'Julio/2019',
     cargo:'Co-fundador y CFO',
-    empresa:'Undervisa Master Trade',
     actividad:[
       '-Planificación financiera',
       '-Administración de redes sociales',
@@ -21,7 +21,52 @@ export class CVComponent implements OnInit {
       '-Elaboracion y administracion de Paginas Web'
     ],
     logo:'../assets/Undervisa.svg'
-    }
+    },
+    {empresa:'Freelancer',
+      fechaInicio: 'Noviembre/2014',
+    fechaFin:'Marzo/2019',
+    cargo:'Consultor Kactus HCM',
+    actividad:[
+      '-Proyectos de soporte e implementación de Kactus HCM (nomina y RRHH)',
+      '-Elaboración de reportes SQL y Kactus Report ',
+      '-Asesorías en legislación laboral',
+      '-Elaboración informes de UGPP y liquidacion de planillas de correcion',
+      '-Auditorias Nomina ',
+      '-Liquidación de nomina y seguridad sociales',
+      '-Interfaz contable',
+      '-Optimización de procesos '
+    ],
+    logo:'../assets/LogoRedondo.svg'
+    },
+    {empresa:'DigitalWare',
+      fechaInicio: 'Abril/2014',
+    fechaFin:'Octubre/2014',
+    cargo:'Consultor Senior Kactus HCM',
+    actividad:[
+      '-Soporte Técnico y funcional',
+      '-Atención casos clientes',
+      '-Replicas Errores',
+      '-Calidad Producto',
+      '-Consulta en todos los motores' ,
+      '-Consulta, capacitación y soporte a clientes y consultores',
+      '-Implementación aplicació'
+    ],
+    logo:'../assets/Undervisa.svg'
+    },
+    {empresa:'Undervisa Master Trade',
+      fechaInicio: 'Agosto/2018',
+    fechaFin:'Julio/2019',
+    cargo:'Co-fundador y CFO',
+    actividad:[
+      '-Planificación financiera',
+      '-Administración de redes sociales',
+      '-Elaboracion  de campañas de marketing',
+      '-Elaboraciones de planes de Negocios',
+      '-Desarrollo de web application PWA',
+      '-Elaboracion y administracion de Paginas Web'
+    ],
+    logo:'../assets/Undervisa.svg'
+    },
   ]
   constructor() { }
 
