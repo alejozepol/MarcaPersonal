0d924655a7c583c6980caef3db6600557cbb96e2
alejozepol Alejandro López Ramírez <alejozepol@gmail.com>
2020-01-24 16:46:56 -0500

    footer

--- src/container/footer/index.jsx
+++ src/container/footer/index.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 /* import { Link } from 'react-router-dom'; */
 import IconMegadono from '../../assets/static/megafono.svg';
 import Logo from '../../assets/static/LogoRedondo.svg';
+import IconContactame from '../../assets/static/Contactame.svg';
 import './Footer.scss';
+import TextIcon from '../../components/TextIcon';
 
 const Footer = () => {
   return (
@@ -10,7 +12,7 @@ const Footer = () => {
       <div className='Footer__suscription'>
         <img className='Footer__suscription-img' src={IconMegadono} alt='icono megafono' />
         <h3 className='Footer__suscription-text'>Cuentame de emprendimiento, tecnología y nuevos proyectos</h3>
-        <input className='Footer__suscription-input' type='email' />
+        <input className='Footer__suscription-input' type='email' placeholder='Correo electronico' />
         <button className='Footer__suscription-buton' type='submit'>Suscribirme</button>
       </div>
       <div className='Footer__brand'>
@@ -20,10 +22,15 @@ const Footer = () => {
           <span role='img' aria-labelledby='corazon'>
             💖
           </span>
-           y pasion
+          y pasion
         </p>
         <p className='Footer__brand-title'>@alejozepol</p>
       </div>
+{/*       <div className='Footer__contact'>
+        <TextIcon img={IconContactame} name='Contactame' />
+        <TextIcon img={IconContactame} name='@Alejozepol' />
+        <TextIcon img={IconContactame} name='Contactame' />
+      </div> */}
     </footer>
   );
 };
