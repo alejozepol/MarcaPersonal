37ce697231cfa9cb07110cdb1d803d2491a8f03a
alejozepol Alejandro López Ramírez <alejozepol@gmail.com>
2020-01-24 18:17:34 -0500

    menu hamburgesa responsive

--- src/container/footer/index.jsx
+++ src/container/footer/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 /* import { Link } from 'react-router-dom'; */
 import IconMegadono from '../../assets/static/megafono.svg';
 import Logo from '../../assets/static/LogoRedondo.svg';
-import IconContactame from '../../assets/static/Contactame.svg';
+/* import IconContactame from '../../assets/static/Contactame.svg'; */
 import './Footer.scss';
-import TextIcon from '../../components/TextIcon';
+/* import TextIcon from '../../components/TextIcon'; */
 
 const Footer = () => {
   return (
@@ -12,8 +12,10 @@ const Footer = () => {
       <div className='Footer__suscription'>
         <img className='Footer__suscription-img' src={IconMegadono} alt='icono megafono' />
         <h3 className='Footer__suscription-text'>Cuentame de emprendimiento, tecnología y nuevos proyectos</h3>
-        <input className='Footer__suscription-input' type='email' placeholder='Correo electronico' />
-        <button className='Footer__suscription-buton' type='submit'>Suscribirme</button>
+        <form action='https://facebook.us20.list-manage.com/subscribe/post?u=b7f408a304fd50944e1238192&amp;id=e7dbf243c0' method='post' id='mc-embedded-subscribe-form' name='mc-embedded-subscribe-form' target='_blank'>
+          <input name='EMAIL' id='mce-EMAIL' className='Footer__suscription-input' type='email' placeholder='Correo electronico' />
+          <button className='Footer__suscription-buton' type='submit'>Suscribirme</button>
+        </form>
       </div>
       <div className='Footer__brand'>
         <img className='Footer__brand-img' src={Logo} alt='Logo Alejandro López | Alejozepol' />
@@ -26,7 +28,7 @@ const Footer = () => {
         </p>
         <p className='Footer__brand-title'>@alejozepol</p>
       </div>
-{/*       <div className='Footer__contact'>
+      {/*       <div className='Footer__contact'>
         <TextIcon img={IconContactame} name='Contactame' />
         <TextIcon img={IconContactame} name='@Alejozepol' />
         <TextIcon img={IconContactame} name='Contactame' />
--- src/container/header/index.jsx
+++ src/container/header/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 /* import { Link } from 'react-router-dom'; */
 import TextIcon from '../../components/TextIcon';
 import IconoInicio from '../../assets/static/IconoInicio.svg';
@@ -28,18 +28,44 @@ const Header = (props) => {
         img: IconoPortafolio,
       },
     ];
-
+  const [bmenu, setBMenu] = useState(false);
+  const handleMenu = () => {
+    bmenu ? setBMenu(false) : setBMenu(true)
+  }
   return (
     <header className='Header'>
       <div className='Header__logo'>
         <img className='Header__logo-img' src={Logo} alt='Logo Alejandro López | Alejozepol' />
         <h1 className='Header__logo-title'>Alejandro López | Alejozepol</h1>
       </div>
-      <nav>
-        {
-          menu.map((item) => <TextIcon key={item.name} img={item.img} name={item.name} />)
-        }
-      </nav>
+      {
+        bmenu ? (
+          <button type='button' className='Header__menu' onClick={() => handleMenu()}>
+            <i className='material-icons'>
+              menu_open
+            </i>
+          </button>
+        ) :
+          (
+            <button type='button' className='Header__menu' onClick={() => handleMenu()}>
+              <i className='material-icons'>menu</i>
+            </button>
+          )
+      }
+      {bmenu && (
+        <nav className='Header__nav700'>
+          {
+            menu.map((item) => <TextIcon key={item.name} img={item.img} name={item.name} />)
+          }
+        </nav>
+      )}
+      {
+        <nav className='Header__nav701'>
+          {
+            menu.map((item) => <TextIcon key={item.name} img={item.img} name={item.name} />)
+          }
+        </nav>
+      }
     </header>
   );
 };
