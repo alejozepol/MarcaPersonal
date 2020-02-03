17aaf6e4d70ddafa6b1e368397abb3a68f5f5fd0
alejozepol Alejandro López Ramírez <alejozepol@gmail.com>
2020-02-03 09:53:54 -0500

    portafolio

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Card.scss';
 
-const Card = ({ img, title, text }) => {
+const Card = ({ img, title, text, children }) => {
   return (
     <section className='Card'>
       <img src={img} alt={title} />
       <h3>{title}</h3>
       <p>{text}</p>
+      {children}
     </section>
   );
 };
