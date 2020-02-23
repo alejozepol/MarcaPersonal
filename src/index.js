4e67f538f310182c0feb570d5e8afcde8cb39775
alejozepol Alejandro López Ramírez <alejozepol@gmail.com>
2020-02-23 16:41:52 -0500

    ejecucion en produccion,

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Platzi from './platzi';
 import { FirebaseContext, Firebase } from './services/firebase';
 import App from './routes';
 
