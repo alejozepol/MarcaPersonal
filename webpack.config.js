ac99a55d1889efef4de92fe02a28f123831b4ae9
Alejandro López Ramírez <alejozepol@gmail.com>
2020-06-23 18:49:16 -0500

    correcion favicon extencion

--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,7 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: './public/index.html',
       filename: './index.html',
-      favicon: './public/favicon.png',
+      favicon: './public/favicon.ico',
     }),
     new MiniCssExtraxtPlugin({
       filename: nodeEnv ? 'assets/[name].css' : 'assets/[hash].css',
