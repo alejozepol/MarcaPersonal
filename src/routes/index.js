7c3ee89687bcc1cc6d512b8e506879022d10683a
Alejandro López Ramírez <alejozepol@gmail.com>
2020-06-23 11:42:24 -0500

    prueba deploy QA

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,7 +9,7 @@ import '../styles/app.scss';
 
 const App = () => {
   return (
-    <BrowserRouter basename='/MarcaPersonal'>
+    <BrowserRouter>
       <Switch>
         <Layout>
           <Route exact path='/' component={Home} />
